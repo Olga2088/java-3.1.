3c0915251393689a908ee349cf25097409ba9a6a
Olga2088 <beltyukovaon@gmail.com>
2020-09-07 14:30:44 +0500

    Main.java comitted

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 7845;
+        int bonusEquivalent = 20;
+        float bonusMiles = ticketPrice / bonusEquivalent;
+        System.out.println(bonusMiles);
+    }
+}
